bc5bb337675751f7113bbe51f33c8f24901fb35a
Bernd Stramm <bernd.stramm@gmail.com>
2011-01-08 21:27:29 -0500

    more [packing

--- pack.sh
+++ pack.sh
@@ -9,6 +9,7 @@ PACKDIR=${HOME}/packaging/crawl
 makearchive.sh ${NAME}-${VERSION} master
 cp ${NAME}-${VERSION}.tar.gz ${PACKDIR}
 cp ${NAME}.desktop ${PACKDIR}
+cp ${NAME}.png ${PACKDIR}
 echo ${NAME} > ${PACKDIR}/pack-name
 echo ${VERSION} > ${PACKDIR}/pack-version
 ls -l ${PACKDIR}/${NAME}-${VERSION}.tar.gz
